f6231749211cccca1d65fe28ba66025b6cfa9b86
MSDoshev <mihaildo@outlook.com>
2023-03-25 13:18:49 +0200

    Add Navigation and Homepage
    html and css only

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,22 +1,20 @@
 import { Link } from "react-router-dom";
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
 export const Header = () => {
-
-    return(
-  
-        <header className={styles.main}>
-            <nav >
-                <ul className={styles.navLinks}>
-                    <li><Link to='/' >Home</Link></li>
-                    <li><Link to="/create" >Create</Link></li>
-                    <li><Link to="/about" >Gallery</Link></li>
-                    <li><Link to="/about" >About</Link></li>
-                    <li><Link to="/about" >Login</Link></li>
-                    <li><Link to="/about" >Register</Link></li>
-                    <li><Link to="/about" >Logout</Link></li>
-                </ul>
-            </nav>
-        </header>
-   
-    );
-} 
+  return (
+    <nav className={styles.main}>
+        <p className={styles.logo}>
+           <Link to="/">SchoolApp</Link> 
+        </p>
+      <ul className={styles.navLinks}>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/create">Create</Link></li>
+        <li><Link to="/gallery">Gallery</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/register">Register</Link></li>
+        <li><Link to="/logout">Logout</Link></li>
+      </ul>
+    </nav>
+  );
+};
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,58 +1,26 @@
+import { Link } from "react-router-dom";
+
 import styles from "./Home.module.css"
 export const Home = () => {
     return(
-        <section id="welcome-world" className={styles.main}>
+        <section className={styles.main}>
            
-        <div >
-            <h2>Welcome href our school portal</h2>
+            <div >
+                <h1>Welcome to our school portal</h1>
+                <Link to="/about">About Us</Link>
+            </div>
+
+            <article>
+                <h2>Gallery</h2>
+                <Link to="/gallery">Go to Gallery</Link>
+            </article>
+            <article>
+                <h2>Students</h2>
+                <Link to="/students">Go to Students</Link>
+            </article>
+
             
-        </div>
-        <img src="./images/four_slider_img01.png" alt="hero"/>
-
-        <div id="home-page">
-            <h1>Latest Games</h1>
-
-            {/* <!-- Display div: with information about every game (if any) --> */}
-            <div className="game">
-                <div className="image-wrap">
-                    <img src="./images/CoverFire.png"/>
-                </div>
-                <h3>Cover Fire</h3>
-                <div className="rating">
-                    <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-                </div>
-                <div className="data-buthrefns">
-                    <a href="#" className="btn details-btn">Details</a>
-                </div>
-            </div>
-            <div className="game">
-                <div className="image-wrap">
-                    <img src="./images/ZombieLang.png"/>
-                </div>
-                <h3>Zombie Lang</h3>
-                <div className="rating">
-                    <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-                </div>
-                <div className="data-buthrefns">
-                    <a href="#" className="btn details-btn">Details</a>
-                </div>
-            </div>
-            <div className="game">
-                <div className="image-wrap">
-                    <img src="./images/MineCraft.png"/>
-                </div>
-                <h3>MineCraft</h3>
-                <div className="rating">
-                    <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-                </div>
-                <div className="data-buthrefns">
-                    <a href="#" className="btn details-btn">Details</a>
-                </div>
-            </div>
-
-            {/* <!-- Display paragraph: If there is no games  --> */}
-            <p className="no-articles">No games yet</p>
-        </div>
-    </section>
+           
+        </section>
     );
 }
