31ac9c5c25aac4218ecb2352bfb4c2ca801ce3c8
MSDoshev <mihaildo@outlook.com>
2023-04-07 20:08:34 +0300

    Fix gallery add button

--- client/src/components/Gallery/Gallery.js
+++ client/src/components/Gallery/Gallery.js
@@ -15,10 +15,12 @@ export const Gallery = ({
 
     return (
         <section className={styles.galleryPage}>
+            <div className={styles.title}>
             <h1>Gallery</h1>
             {isAuthenticated && (
             <Link to='/gallery/create'>+</Link>
             )}
+            </div>
             <div className={styles.allPictures}>
                 {pictures.map(x => <img src={x.imageUrl}/>)}
                 
